4d9a5bd751a1f8757df80b9cc9b726a3282f82fa
王艺锦 <wang.yijin@foxmail.com>
2018-09-12 23:30:10 +0800

    单例模式

--- 单例模式/demo.js
+++ 单例模式/demo.js
@@ -1,9 +1,11 @@
 /**
  * 单例模式
+ * @param {Function} fun 执行函数
+ * @returns {Object} 执行函数的返回值
  */
-function singleton({ fn }) {
+function singleton({ fun }) {
   let res
   return function() {
-    return res || (res = fn.apply(this, arguments))
+    return res || (res = fun.apply(this, arguments))
   }
 }
